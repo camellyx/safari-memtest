98fe37621e6af7933d513ce77dead6ecb25153c1
Nolan Dickey <nolandickey@gmail.com>
2015-06-11 14:50:16 -0400

    New logs, modified SummAddr to give frequencies for addresses, rows, and columns where errors occured.

--- QuickSummary/SummAddr.java
+++ QuickSummary/SummAddr.java
@@ -4,7 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
 import java.text.ParseException;
-import java.util.ArrayList;
+import java.util.*;
 public class SummAddr{
     public static void main(String[] args){
         String newFile = "default.summary";
@@ -14,6 +14,9 @@ public class SummAddr{
             java.util.Date startTime;
             Scanner scanny = new Scanner(System.in);
             ArrayList<File> files = new ArrayList<>();
+            Map<Long, Integer> addrs = new HashMap<Long, Integer>();
+            Map<Long, Integer> rows = new HashMap<Long, Integer>();
+            Map<Long, Integer> cols = new HashMap<Long, Integer>();
             if (args.length!=0){
                 int i = 0;
                 for (String s:args){
@@ -22,7 +25,7 @@ public class SummAddr{
                 }
             }else{
                 System.out.print("Enter log file names in order to be read. Enter blank line to quit: ");
-                String input = "";
+                String input = " ";
                 for (int i = 0; !input.equals(""); i++){
                     input = scanny.nextLine();
                     if (input.equals(""))
@@ -30,10 +33,10 @@ public class SummAddr{
                     files.add(new File(input));
                 }
             }
-            //System.out.print("Enter name of output file for summary: ");
-            //newFile = scanny.nextLine();
-            //PrintWriter write = new PrintWriter(newFile);
-            //BufferedWriter bw = new BufferedWriter(write);
+            System.out.print("Enter name of output file for summary: ");
+            newFile = scanny.nextLine();
+            PrintWriter write = new PrintWriter(newFile);
+            BufferedWriter bw = new BufferedWriter(write);
             String status;
             for (File file:files){
                 BufferedReader br = new BufferedReader(new FileReader(file));
@@ -45,19 +48,51 @@ public class SummAddr{
                     if (line.contains("MC10_ADDR")){
                         status = line.substring(line.indexOf('=')+1, line.length());
                         long addr = Long.parseLong(status, 16);
+                        if (addrs.containsKey(addr))
+                            addrs.put(addr, addrs.get(addr)+1);
+                        else
+                            addrs.put(addr, 1);
                         long row = addr/8192;
+                        if (rows.containsKey(row))
+                            rows.put(row, rows.get(row)+1);
+                        else
+                            rows.put(row, 1);
                         long column = addr%8192;
-                        String rs = String.valueOf(row);
-                        String cs = String.valueOf(column);
-                        status = rs + " " + cs + "\n";
-                        System.out.print(status);
-                        //bw.write(status);
-                        //bw.newLine();
+                        if (cols.containsKey(column))
+                            cols.put(column, cols.get(column)+1);
+                        else
+                            cols.put(column, 1);
                     }
                 }
                 br.close();
             }
-            //bw.close();
+            Comparator com = new Comparator(){
+                public int compare(Object obj1, Object obj2){
+                    return (((Comparable)(((Map.Entry) obj1).getValue())).compareTo((Comparable)(((Map.Entry) obj2).getValue())));
+                }
+            };
+            List aList = new LinkedList(addrs.entrySet());
+            Collections.sort(aList, com);
+            List rList = new LinkedList(rows.entrySet());
+            Collections.sort(rList, com);
+            List cList = new LinkedList(cols.entrySet());
+            Collections.sort(cList, com);
+            bw.write("Addresses:\n");
+            for (Iterator<Map.Entry> it = aList.iterator();it.hasNext();){
+                Map.Entry ent = it.next();
+                bw.write(ent.getKey()+": "+ent.getValue()+" errors.\n");
+            }
+            bw.write("Rows:\n");
+            for (Iterator<Map.Entry> it = rList.iterator();it.hasNext();){
+                Map.Entry ent = it.next();
+                bw.write(ent.getKey()+": "+ent.getValue()+" errors.\n");
+            }
+            bw.write("Columns:\n");
+            for (Iterator<Map.Entry> it = cList.iterator();it.hasNext();){
+                Map.Entry ent = it.next();
+                bw.write(ent.getKey()+": "+ent.getValue()+" errors.\n");
+            }
+            bw.close();
         }
         catch(FileNotFoundException ex){
             System.out.println("Unable to open file");
